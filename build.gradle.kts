f11b096ab7a3618f4447d341d1ffa1944726325b
DerBot7214 <github@dertyp.dev>
2026-01-15 14:19:23 +0100

    fix android

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,16 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
-    id("org.jetbrains.kotlinx.rpc.plugin") version "0.10.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlinx.rpc)
 }
 
 group = "de.dertyp7214"
 version = "1.0.0"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    api("org.jetbrains.kotlinx:kotlinx-rpc-core:0.10.1")
-    api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.0")
-    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    api(libs.kotlinx.rpc.core)
+    api(libs.kotlinx.serialization.core)
+    api(libs.kotlinx.serialization.json)
 }
 
 kotlin {
