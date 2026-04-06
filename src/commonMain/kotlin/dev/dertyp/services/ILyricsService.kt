18c8a9a5f8592fce576921282d858f3379884778
DerBot7214 <github@dertyp.dev>
2026-04-06 16:50:47 +0200

    add configuration check for lyrics service skip lyrics sync worker execution if service is unconfigured refactor sync worker to use concrete LyricsService implementation

--- src/commonMain/kotlin/dev/dertyp/services/ILyricsService.kt
+++ src/commonMain/kotlin/dev/dertyp/services/ILyricsService.kt
@@ -9,4 +9,5 @@ interface ILyricsService {
     suspend fun getSyncedLyrics(songId: PlatformUUID): SyncedLyrics?
     suspend fun transcribeLyrics(songId: PlatformUUID, lyrics: String? = null): SyncedLyrics?
     suspend fun startSyncWorker(): Boolean
+    suspend fun isConfigured(): Boolean
 }
